638c4de33bade2e1d03684b8cfb6ca8e6dd67ea2
lucaslov <czyzukasz2402@gmail.com>
2023-03-10 15:33:11 +0100

    Users Express App

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+const port = 3000; // localhost:3000
+
+let users = [
+    {
+        id: 1,
+        name: 'Adam',
+        surname: 'Kowalski'
+    },
+    {
+        id: 2,
+        name: 'Tomasz',
+        surname: 'Ktosialski'
+    },
+    {
+        id: 3,
+        name: 'Tomasz',
+        surname: 'Ktosialski2'
+    }
+]
+
+app.get('/users', (request, result) => {
+    console.log(request);
+    result.status(200).send(users);
+});
+
+app.post('/users', (request, result) => {
+    console.log(request);
+    users.push(request.body);
+    result.status(200).send(users);
+});
+
+app.delete('/users/:id', (request, result) => {
+    const id = parseInt(request.params.id);
+    const indexOfUser = users.indexOf(user => user.id === id);
+    delete users[indexOfUser];
+    result.status(204).send('Deleted, thanks!');
+})
+
+app.listen(port, () => {
+    console.log(`Server started!! on port ${port}`);
+});
